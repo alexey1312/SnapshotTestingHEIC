54c8d54d99a2c3961fda2da1196ce572911a8448
Aleksei Kakoulin <alexey1312ru@gmail.com>
2022-01-18 16:38:01 +0300

    Add new CompressionQuality values

--- Sources/SnapshotTestingHEIC/HEIC/CompressionQuality.swift
+++ Sources/SnapshotTestingHEIC/HEIC/CompressionQuality.swift
@@ -12,15 +12,23 @@ public struct CompressionQuality: ExpressibleByFloatLiteral {
     public init(_ value: Float) {
         self.value = value
     }
-    
+
     public static var lossless: CompressionQuality {
         return 1.0
     }
-    
+
+    public static var low: CompressionQuality {
+        return 0.8
+    }
+
     public static var medium: CompressionQuality {
         return 0.5
     }
-    
+
+    public static var high: CompressionQuality {
+        return 0.2
+    }
+
     public static var maximum: CompressionQuality {
         return 0.0
     }
